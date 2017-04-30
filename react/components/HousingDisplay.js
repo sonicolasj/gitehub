1781465efd2099eebd22e223c94841bc0c0012e4
Nicolas JANSSOONE <nicolas.janssoone@gmail.com>
2017-04-30 22:14:43 +0200

    Ajout de la création d'évenement calendrier

--- react/components/HousingDisplay.js
+++ react/components/HousingDisplay.js
@@ -9,11 +9,15 @@ import {
   StyleSheet
 } from 'react-native'
 
+import RNCalendarEvents from 'react-native-calendar-events';
+
 export default class HousingDisplay extends React.Component {
     constructor(props) {
         super(props);
 
         this.props.housing = this.props.housing || { listing: {}, pricing_quote: {} };
+
+        this.addToCalendar = this.addToCalendar.bind(this);
     }
 
     render() {
@@ -24,7 +28,19 @@ export default class HousingDisplay extends React.Component {
                     source={{uri: this.props.housing.listing.picture_url}} />
 
                 <Text>{this.props.housing.listing.name}</Text>
+
+                <Button title="Ajouter à l'agenda" onPress={() => this.addToCalendar()} />
             </View>
         );
+    }
+
+    addToCalendar() {
+        let housing = this.props.housing;
+
+        RNCalendarEvents.saveEvent(housing.listing.name, {
+            location: housing.listing.public_address,
+            startDate: housing.pricing_quote.check_in,
+            endDate: housing.pricing_quote.check_out
+        });
     }
 }
